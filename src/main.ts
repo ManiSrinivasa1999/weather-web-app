cab232052d835112099653719dac08f771da6d9a
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2021-01-24 08:34:43 +0530

    add vuetify, fontawesome, searchbar

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faSearch,
+  faMapMarkerAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+library.add(
+  faSearch,
+  faMapMarkerAlt,
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="search-box">
+      <v-text-field
+        v-model="search"
+        label="Search..."
+        solo
+        @keypress="fetchWeather"
+        class="search-bar rounded-lg"
+        append-icon="fas fa-search"
+        prepend-inner-icon="fas fa-map-marker-alt"
+      ></v-text-field>
+    </div>
+    <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+      <div class="location-box">
+        <div class="location">
+          {{ weather.name }}, {{ weather.sys.country }}
+        </div>
+        <div class="date">{{ dateBuilder() }}</div>
+      </div>
+
+      <div class="weather-box">
+        <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+        <div class="weather">{{ weather.weather[0].main }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      urlBase: 'https://api.openweathermap.org/data/2.5/',
+      apiKey: '3e7c22fddbf2e104d77b9e54040865d0',
+      search: '',
+      weather: {},
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key === 'Enter') {
+        fetch(
+          `${this.urlBase}weather?q=${this.search}&units=metric&APPID=${this.apiKey}`,
+        )
+          .then((res) => res.json())
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.search = '';
+      this.weather = results;
+    },
+    dateBuilder() {
+      const d = new Date();
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
+      const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ];
+      const day = days[d.getDay()];
+      const date = d.getDate();
+      const month = months[d.getMonth()];
+      const year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    },
+  },
+};
 </script>
